d82b53339577b381279ac97f1cd791c5568cd3ee
Margarita Rudenko <ritastar2005@mail.ru>
2021-10-31 21:20:57 +0500

    Example005_ConditionIfElse project

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
