21f0bdf251ca863a2e24df928b07dfe1b0ce6443
Margarita Rudenko <ritastar2005@mail.ru>
2021-11-01 18:30:52 +0500

    Example008_FriendsAndDog project

---
+++ Examples/Example008_FriendsAndDog/Program.cs
@@ -0,0 +1,28 @@
+﻿double distance = 10000;
+double firstFriendSpeed = 2;
+double secondFriendSpeed = 4;
+double dogSpeed = 6;
+double time = 0;
+int friend = 2;
+
+double count = 0;
+
+while(distance > 2)
+{
+        if(friend == 1)
+    {
+        time = distance/(firstFriendSpeed+dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(secondFriendSpeed+dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed)*time;
+    count++;
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз между друзьями");
